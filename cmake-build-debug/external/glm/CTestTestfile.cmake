109574f7432288492e3727acdd57e88ba4924912
Ivan Voronov <ivvoronov2308@gmail.com>
2023-04-05 21:49:10 +0300

    Refactor

--- cmake-build-debug/external/glm/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/ivanvoronov/Documents/Учеба/Programming_4th_semester/serpinsky_cemetery/external/glm
-# Build directory: /Users/ivanvoronov/Documents/Учеба/Programming_4th_semester/serpinsky_cemetery/cmake-build-debug/external/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("glm")
